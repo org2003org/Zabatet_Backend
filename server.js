f89b0e7c2440ea7895b2107a73cf662523bb225c
ziad <zeyadkabeel99@gmail.com>
2026-06-08 19:16:31 +0300

    adding cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { connectDB } from "./config/db.js";
 import dotenv from "dotenv";
 import mainRouter from "./routes/index.js";
@@ -7,7 +8,10 @@ import errorHandler from "./middlewares/errorHandler.js";
 dotenv.config();
 
 const app = express();
-
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
