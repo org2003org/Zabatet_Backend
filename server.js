292741c579a16a86884ba8063f2ff78979c41110
Mahmoud Hassan <mahmoudhassan10191019@gmail.com>
2026-06-12 20:24:19 +0300

    add headers to cors

--- server.js
+++ server.js
@@ -11,7 +11,8 @@ dotenv.config();
 const app = express();
 app.use(cors({
   origin: "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }));
 const PORT = process.env.PORT || 3000;
